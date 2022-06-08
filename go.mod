49bbbefd2c3608cb93fab1450bff2c89864d8b49
Jose Emmanuel Encarnacion Santana <jose.encarnacion@hit.com.do>
2022-06-08 10:45:28 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ramses2099/gowebservice
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//REST (Representation State Transfer)
+
+// Handling HTTP Requests
+// -GET, PUT, POST, DELETE
+// -Encoding Data
+func main() {
+
+	fmt.Println("hello")
+}
